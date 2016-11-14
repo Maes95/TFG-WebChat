fcb7f1b38fdc84b57288cc5defa2649a9c141592
Maes95 <michel.maes95@gmail.com>
2016-11-15 00:58:31 +0100

    Simple chat and add rooms

--- WebChatNode/public/app/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'my-app',
-    directives: [],
-    templateUrl: '../app/app/template.html'
-})
-export class AppComponent { }
