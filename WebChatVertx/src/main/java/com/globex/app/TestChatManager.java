75dadf4930155d0059c561082cac2ee09357bfd7
Maes95 <michel.maes95@gmail.com>
2016-11-19 04:05:19 +0100

    New Vertx3 ChatManager

--- WebChatVertx/src/main/java/com/globex/app/TestChatManager.java
+++ WebChatVertx/src/main/java/com/globex/app/TestChatManager.java
@@ -8,8 +8,6 @@ package com.globex.app;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.ServerWebSocket;
 
 /**
  *
@@ -19,30 +17,13 @@ public class TestChatManager {
   
   public static void main(String[] args) {
     
-    // SERVER SIDE
-    
-    HttpServer server = Vertx.vertx().createHttpServer();
-    
-    server.websocketHandler((ServerWebSocket webs) -> {
-        System.out.println("Client connected");
-        webs.writeBinaryMessage(Buffer.buffer("Hello user"));
-        System.out.println("Client's message: ");
-        webs.handler(data -> {System.out.println("Received data " + data.toString("ISO-8859-1"));});
-    });
-
-    server.listen(8080, "localhost", res -> {
-        if (res.succeeded()) {
-            System.out.println("Server is now listening!");
-        } else {
-            System.out.println("Failed to bind!");
-        }
-    });    
+    Runner.runExample(ChatManager2.class);
     
     // CLIENT SIDE
             
     HttpClient client = Vertx.vertx().createHttpClient();
 
-    client.websocket(8080, "localhost", "/eventbus/", websocket -> {
+    client.websocket(8080, "localhost", "/chat", websocket -> {
         websocket.handler(data -> {
             System.out.println("Server message: ");
             System.out.println("Received data " + data.toString("ISO-8859-1"));
---
+++ WebChatVertx/src/main/java/com/globex/app/User.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.globex.app;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+/**
+ *
+ * @author michel
+ */
+
+public class User extends AbstractVerticle{
+	
+	String name;
+	String chat;
+	
+	public User(String name, String chat) {
+            this.name = name;
+            this.chat = chat;
+	}
+	
+        @Override
+	public void start(){
+            vertx.eventBus().consumer(chat).handler( data -> {
+                
+                JsonObject message = (JsonObject) data.body();
+                JsonObject respuesta = new JsonObject();
+                respuesta.put("name", message.getString("user"));
+                respuesta.put("color", message.getString("color"));
+                respuesta.put("message", message.getString("message"));
+                respuesta.put("sender", name);
+
+                vertx.eventBus().send(name, respuesta);
+            });
+	}
+}
